62fa610e7c976f4447dee7879fa7c1261ef01653
Charalampos (Babis) Sarantoglou <babis.sarantoglou@gmail.com>
2019-05-04 19:29:37 +0300

    Remove the logic that plays exactly five rounds

--- code.js
+++ code.js
@@ -25,25 +25,8 @@ function game() {
     let computerWins = 0,
         playerWins = 0,
         playerSelection;
-    for (let i=0; i<5; i++) {
-        do {
-        playerSelection = prompt('Select "Rock", "Paper", or "Scissors"');
-        if (typeof playerSelection != 'string') {
-            console.log("Game cancelled!");
-            return;
-        }
-        playerSelection = playerSelection.charAt(0).toUpperCase()
-                        + playerSelection.substring(1).toLowerCase();
-        } while (playerSelection != 'Rock' && playerSelection != 'Paper' && playerSelection != 'Scissors');
-        let roundResult = playRound(playerSelection, computerPlay());
-        console.log(roundResult);
-        if (roundResult.indexOf("Lose") == 4) {
-           computerWins++;
-        } else if (roundResult.indexOf("Win") == 4) {
-            playerWins++;
-        } 
-    }
+
     console.log(computerWins > playerWins ? "The computer is the winner!" :
-                playerWins > computerWins ? "You are the winner!" :
-                                            "You and the computer drew!");
+            playerWins > computerWins ? "You are the winner!" :
+                                        "You and the computer drew!");
 }
